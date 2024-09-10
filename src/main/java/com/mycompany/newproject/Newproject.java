875e18f9d7bdc0ffbcc68b2bc8412a549197c5db
RC_Student_lab <RC_Student_lab@10.0.0.92>
2024-09-10 12:05:51 +0000

    Created an application that adds numbers

--- src/main/java/com/mycompany/newproject/Newproject.java
+++ src/main/java/com/mycompany/newproject/Newproject.java
@@ -11,6 +11,10 @@ package com.mycompany.newproject;
 public class Newproject {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int a=5;
+        int b=10;
+        int c= a+b;
+        System.out.println( c);
+        
     }
 }
